de85a0144dc92a7c367b51221b54bdcd74642690
Btulika <500084883@stu.upes.ac.in>
2023-08-16 23:52:21 +0530

    Typecasting

--- Typecasting.java
+++ Typecasting.java
@@ -3,14 +3,25 @@ import java.util.Scanner;
 public class Typecasting {
     public static void main(String[] args) {
         Scanner scanner =new Scanner(System.in);
-        float num = scanner.nextFloat();
-        //int num = scanner.nextInt();
-        System.out.println(num);
+    //     float num = scanner.nextFloat();
+    //     //int num = scanner.nextInt();
+    //     System.out.println(num);
 
-       // error :  int num = 65.56f;
-       int num1 = (int)(67.56f); 
-       System.out.println(num1);
+    //    // error :  int num = 65.56f;
+    //    int num1 = (int)(67.56f); 
+    //    System.out.println(num1);
         scanner.close();
+        //automatic type promotion in expressions
+        int a = 257;
+        byte b = (byte)(a); // 257 % 256 = 1
+        System.out.println(b);
+
+        byte m = 40;
+        byte n = 50;
+        byte o = 100;
+        int p = (m * n) / o;
+        System.out.println(p);
+
     }
     
 }
